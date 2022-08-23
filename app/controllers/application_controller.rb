25ae40d753d4a3113f3680e3b6c1ecaacc5067ee
+ <+dannaewaltz@gmail.com>
2022-08-23 16:41:05 -0600

    commit tree b15dc834e214f763ebb6f0d9ce741d3d5b11e815

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < Sinatra::Base
   set default_content_type: "application/json"
+
+  get '/' do
+    "hi"
+  end
   
   get '/bakeries' do
     bakeries = Bakery.all
@@ -21,6 +25,50 @@ class ApplicationController < Sinatra::Base
     # see the BakedGood class for the  method definition of `.by_price`
     baked_good = BakedGood.by_price.first
     baked_good.to_json
+  end
+
+  post "/bakeries" do
+    new_bake = Bakery.create(
+      name: params["name"],
+    )
+    new_bake.to_json
+  end
+
+  post "/baked_goods" do
+    if Bakery.find_by(id: params["bakery_id"])
+    new_baked_good = BakedGood.create(
+      name: params["name"],
+      price: params["price"],
+      bakery_id: params["bakery_id"],
+    )
+    new_baked_good.to_json
+  else
+    {"error": "nice try"}.to_json
+  end
+end
+
+  patch '/bakeries/:id' do
+    bake_to_be_updated = Bakery.find_by(id: params[:id])
+    if bake_to_be_updated
+      bake_to_be_updated.update(name: params[:name])
+      bake_to_be_updated.to_json
+    else
+      {"error": "uhoh"}.to_json
+    end
+  end
+
+  delete '/baked_goods/:id' do
+    found_baked_good = BakedGood.find_by(id: params[:id])
+    found_baked_good.destroy 
+    found_baked_good.to_json 
+    # if found_baked_good
+    #   status 204
+    #   #head :no_content
+    #   #found_baked_good.delete
+    # else
+    #   status 404
+    #   {"error": "uhoh"}.to_json
+    # end
   end
 
 end
